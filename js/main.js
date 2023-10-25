622e0d2c8cd7a116c6c41c68b45b265e99ff388e
Danu248 <148397838+Danu248@users.noreply.github.com>
2023-10-25 18:15:29 +0000

    Added support of 50 rows Changed hashtags to dashes to improve visibility Increased display size of player Changed player movement to be slower

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 let map = [];
 let mapHeight = 30;
 let mapLength = 180;
-let playerX = 0;
-let playerY = 0;
 let shouldMoveRight = false;
 let shouldMoveLeft = false;
 let shouldMoveUp = false;
 let shouldMoveDown = false;
+let nonAirTileDrawn = false
 
 class KeyboardHandler {
   pressedKeys = []
@@ -21,6 +20,22 @@ class KeyboardHandler {
 
 const keyboardHandler = new KeyboardHandler()
 
+class Entity {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+}
+
+class Player extends Entity {
+  constructor(x, y, name) {
+    super(x, y)
+    this.name = name
+  }
+}
+
+const player = new Player(4,2, prompt("Enter Player Name"))
+
 
 //Initiates the default map with sizes indicated by mapHeight and mapLength
 //Initiates random "bushes" as b characters
@@ -28,7 +43,7 @@ for (let i = 0; i < mapHeight; i++) {
     map[i] = [];
     for (let j = 0; j < mapLength; j++) {
       if (Math.floor(Math.random() * 30)) {
-      map[i][j] = "#";
+      map[i][j] = "-";
       } else {
       map[i][j] = "b"
       }
@@ -58,29 +73,49 @@ function drawMap() {
     shouldMoveUp = false;
   }
 
+  //keyboardHandler.pressedKeys = []
+
   if (shouldMoveRight) {
-    playerX += 1
+    player.x += 0.5
   }
   if (shouldMoveLeft) {
-    playerX -= 1
+    player.x -= 0.5
   }
   if (shouldMoveDown) {
-    playerY += 1
+    player.y += 0.25
   }
   if (shouldMoveUp) {
-    playerY -= 1
+    player.y -= 0.25
   }
 
   let rowDisplayValue = ""
 
   for (let i = 0; i < mapHeight; i++) {
       for (let j = 0;j < mapLength; j++) {
-          
-          if (i == playerY && j == playerX) {
+        for (let k = 0;k<3;k++) {
+          if (i == Math.floor(player.y - 1 + k) && j == Math.floor(player.x - 1)) {
             rowDisplayValue = rowDisplayValue + "P"
-          } else {
-            rowDisplayValue = rowDisplayValue + map[i][j]
+            nonAirTileDrawn = true
+          } else if (i == Math.floor(player.y - 1 + k) && j == Math.floor(player.x)) {
+            rowDisplayValue = rowDisplayValue + "P"
+            nonAirTileDrawn = true
+          } else if (i == Math.floor(player.y - 1 + k) && j == Math.floor(player.x + 1)) {
+            rowDisplayValue = rowDisplayValue + "P"
+            nonAirTileDrawn = true
+          } else if (i == Math.floor(player.y - 1 + k) && j == Math.floor(player.x + 2)) {
+            rowDisplayValue = rowDisplayValue + "P"
+            nonAirTileDrawn = true
+          } else if (i == Math.floor(player.y - 1 + k) && j == Math.floor(player.x - 2)) {
+            rowDisplayValue = rowDisplayValue + "P"
+            nonAirTileDrawn = true
           }
+        }
+        //draw 
+        if (!nonAirTileDrawn) {
+          rowDisplayValue = rowDisplayValue + map[i][j]
+        } else {
+          nonAirTileDrawn = false
+        }
       }
       document.getElementById("r" + i).textContent = rowDisplayValue;
       document.getElementById("r" + i).style.fontFamily = "Courier";
@@ -91,6 +126,7 @@ function drawMap() {
   let tempVar = ""
   tempVar = ""
   for (let i = 0; i < keyboardHandler.pressedKeys.length; i++) {tempVar = tempVar + keyboardHandler.pressedKeys[i]}
+  tempVar = tempVar + " x:" + player.x + " y:" + player.y
   document.getElementById("console").textContent = tempVar
   requestAnimationFrame(drawMap)
 }
