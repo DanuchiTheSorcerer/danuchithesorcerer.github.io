1e6a82cfe383f992306776f6d93e2faa3e55c2e6
Danu248 <148397838+Danu248@users.noreply.github.com>
2023-10-25 10:59:49 +0000

    Added movement
    Draws "P" character based on playerX and playerY vars.
    Keyboard handler becomes broken when key repeat is on.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,97 @@
 let map = [];
 let mapHeight = 30;
-let mapLength = 100;
+let mapLength = 180;
+let playerX = 0;
+let playerY = 0;
+let shouldMoveRight = false;
+let shouldMoveLeft = false;
+let shouldMoveUp = false;
+let shouldMoveDown = false;
 
+class KeyboardHandler {
+  pressedKeys = []
+  constructor() {
+    window.addEventListener("keydown", event => this.pressedKeys.push(event.code))
+    window.addEventListener("keyup", event => this.pressedKeys.splice(this.pressedKeys.indexOf(event.code), 1))
+  }
+  getKeys(param) {
+    return this.pressedKeys.indexOf(param) != -1;
+  }
+}
+
+const keyboardHandler = new KeyboardHandler()
+
+
+//Initiates the default map with sizes indicated by mapHeight and mapLength
+//Initiates random "bushes" as b characters
 for (let i = 0; i < mapHeight; i++) {
     map[i] = [];
     for (let j = 0; j < mapLength; j++) {
+      if (Math.floor(Math.random() * 30)) {
       map[i][j] = "#";
+      } else {
+      map[i][j] = "b"
+      }
     }
 }
 
-map[10][10] = "P"
+//DRAW THE MAP
+function drawMap() {
+  if (keyboardHandler.getKeys("KeyA")) {
+    shouldMoveLeft = true;
+  } else {
+    shouldMoveLeft = false;
+  }
+  if (keyboardHandler.getKeys("KeyD")) {
+    shouldMoveRight = true;
+  } else {
+    shouldMoveRight = false;
+  }
+  if (keyboardHandler.getKeys("KeyS")) {
+    shouldMoveDown = true;
+  } else {
+    shouldMoveDown = false;
+  }
+  if (keyboardHandler.getKeys("KeyW")) {
+    shouldMoveUp = true;
+  } else {
+    shouldMoveUp = false;
+  }
 
-let rowDisplayValue = ""
+  if (shouldMoveRight) {
+    playerX += 1
+  }
+  if (shouldMoveLeft) {
+    playerX -= 1
+  }
+  if (shouldMoveDown) {
+    playerY += 1
+  }
+  if (shouldMoveUp) {
+    playerY -= 1
+  }
 
-for (let i = 0; i < mapHeight; i++) {
-    for (let j = 0;j < mapLength; j++) {
-        rowDisplayValue = rowDisplayValue + map[i][j]
-    }
-    document.getElementById("r" + i).textContent= rowDisplayValue;
-    document.getElementById("r" + i).style.fontFamily = "Courier";
-    rowDisplayValue = ""
+  let rowDisplayValue = ""
+
+  for (let i = 0; i < mapHeight; i++) {
+      for (let j = 0;j < mapLength; j++) {
+          
+          if (i == playerY && j == playerX) {
+            rowDisplayValue = rowDisplayValue + "P"
+          } else {
+            rowDisplayValue = rowDisplayValue + map[i][j]
+          }
+      }
+      document.getElementById("r" + i).textContent = rowDisplayValue;
+      document.getElementById("r" + i).style.fontFamily = "Courier";
+      document.getElementById("r" + i).style.fontSize = "10px";
+      rowDisplayValue = ""
+  }
+
+  let tempVar = ""
+  tempVar = ""
+  for (let i = 0; i < keyboardHandler.pressedKeys.length; i++) {tempVar = tempVar + keyboardHandler.pressedKeys[i]}
+  document.getElementById("console").textContent = tempVar
+  requestAnimationFrame(drawMap)
 }
+
